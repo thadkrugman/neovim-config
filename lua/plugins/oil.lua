92f0bae59c189e193213c7c56f64bc6f643dc45a
Thad <hello@thadkrugman.com>
2024-05-28 02:07:58 -0400

    init

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "stevearc/oil.nvim",
+        lazy = false, -- Ensure oil.nvim is not lazy-loaded
+        opts = {
+            keymaps = { ["<Esc>"] = "actions.close", ["<C-s>"] = false },
+            default_file_explorer = true,
+            view_options = {
+                show_hidden = true,
+            },
+            float = {
+                padding = 20,
+            },
+        },
+        config = function(_, opts)
+            -- Disable netrw
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+
+            require("oil").setup(opts)
+        end,
+        keys = {
+            { "<leader>e", "<cmd>Oil --float<CR>", desc = "Explorer" },
+        },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+}
