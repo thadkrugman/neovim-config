b77efe9ef93e3292f0c44294f37865a44d511e96
Thad Krugman <thaddeuskrugman@gmail.com>
2025-03-27 23:43:54 -0400

    .

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -24,33 +24,33 @@ return {
     --     end,
     -- },
     -- Commented out catppuccin configuration
-    -- {
-    --     "catppuccin/nvim",
-    --     name = "catppuccin",
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         require("catppuccin").setup({
-    --             flavour = "frappe", -- latte, frappe, macchiato, mocha
-    --         })
-    --         vim.cmd.colorscheme("catppuccin")
-    --     end,
-    -- },
-
-    -- Dracula configuration with similar structure
     {
-        "Mofiqul/dracula.nvim",
-        name = "dracula",
+        "catppuccin/nvim",
+        name = "catppuccin",
         lazy = false,
         priority = 1000,
         config = function()
-            require("dracula").setup({
-                -- Dracula specific options can go here
-                -- For example:
-                -- transparent_bg = true,
-                -- italic_comment = true,
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
             })
-            vim.cmd.colorscheme("dracula")
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
+
+    -- Dracula configuration with similar structure
+    -- {
+    --     "Mofiqul/dracula.nvim",
+    --     name = "dracula",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("dracula").setup({
+    --             -- Dracula specific options can go here
+    --             -- For example:
+    --             -- transparent_bg = true,
+    --             -- italic_comment = true,
+    --         })
+    --         vim.cmd.colorscheme("dracula")
+    --     end,
+    -- },
 }
